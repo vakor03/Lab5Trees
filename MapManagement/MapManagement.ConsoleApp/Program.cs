27b0654c9accf9f15b0a70c94ffa8a7a9cb2fd20
Valerii_kor <myksv200@gmail.com>
2021-04-21 20:04:56 +0400

    21.04 ConsoleApp
    Created ConsoleApp project with class Program

---
+++ MapManagement/MapManagement.ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using MapManagement.LocationLib;
+using MapManagement.MapLib;
+
+namespace MapManagement.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Location loc1 = new Location("50,60659;30,45436;shop;car;Авто Масло;;");
+            Location loc2 = new Location("50,44075;30,55263;leisure;ice_rink;Льодовий клуб «Піонер»;;");
+            Branch branch = new Branch();
+            branch.AddChild(loc1);
+            branch.AddChild(loc2);
+            List<Node> childs = branch.GetChilds();
+            foreach (var child in childs)
+            {
+                Console.WriteLine((Leaf)child);
+            }
+        }
+    }
+}
